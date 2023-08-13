b682b4c0583b69d0b6725c2be1b1dc245b3eb6b7
Phuc Nguyen <ngqphuc2011@gmail.com>
2023-08-13 11:54:53 +0700

    change transformer

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,6 @@
 const crypto = require("crypto");
 
 module.exports = ({ env }) => ({
-  "users-permissions": {
-    config: {
-      jwtSecret: env("JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
-    },
-  },
   transformer: {
     enabled: true,
     config: {
